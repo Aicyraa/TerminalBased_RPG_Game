35fd7917fc66d001dde1a2eae77f504a7748f31c
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-01-12 20:25:25 +0800

    Utilities

--- src/character/util/Character.java
+++ src/character/util/Character.java
@@ -1,13 +1,19 @@
-package Character;
+package character.util;
 
 public class Character {
-        String name;
+        String user;
+        String heroName;
         int maxHealth;
-        int currentHealth = maxHealth;
+        int currentHealth;
 
-        public void setName(String name) { this.name = name; }
-        public void setMaxHealth(int health) { this.maxHealth = health; }
+        public void setUser(String user) {this.user = user; }
+        public void setHeroName(String name) { this.heroName = name; }
+        public void setMaxHealth(int health) { this.maxHealth = health; this.currentHealth = this.maxHealth; }
 
         public void setDamage(int damage) { this.currentHealth = this.currentHealth - damage; }
+
+        public String getUser() { return this.user; }
+        public String getHeroName() { return this.heroName; }
+        public String getMaxHealth() { return String.valueOf(this.maxHealth); }
 
 }
--- src/character/util/Randomizer.java
+++ src/character/util/Randomizer.java
@@ -1,4 +1,17 @@
 package character.util;
 
 public class Randomizer {
+
+    // health randomizer
+    // attack randomizer
+
+    static int randomNumber(int value) {
+        return (int) (Math.random() * value + 1);
+    }
+
+    public int generateHealth(int health){
+        return health * randomNumber(3);
+    }
+
+
 }
