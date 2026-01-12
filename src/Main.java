3304de4ffbc56f5610edd6fdc07be23de6f74495
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-01-12 19:59:14 +0800

    imported heroes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
+import Heroes.Archer;
+import Heroes.Assasin;
+import Heroes.Mage;
+import Heroes.Warrior;
+
 public class Main {
 
     public static void main(String[] args) {
