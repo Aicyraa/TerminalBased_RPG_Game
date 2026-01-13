2af4c5c00a2bcc1a73e454153634f6e9c3dbdcc0
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-01-13 13:29:11 +0800

    enemyHealth

--- src/character/util/Enemy.java
+++ src/character/util/Enemy.java
@@ -1,4 +1,12 @@
 package character.util;
 
-public class Enemy {
+public class Enemy extends Character{
+
+    private final static int healthRange = 3;
+    private static int randomNumber() { return (int) (Math.random() * healthRange + 1);}
+
+    public void setEnemyHealth(int health){
+        setMaxHealth(health * randomNumber());
+    }
+
 }
